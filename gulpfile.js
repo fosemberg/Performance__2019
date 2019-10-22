9f2b2592002cd637c556aa90838d500b601263fc
fosemberg <fosemberg@gmail.com>
2019-10-22 23:16:32 +0300

    js uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp');
 const inlineCss = require('gulp-inline-css');
 let cleanCSS = require('gulp-clean-css');
+let uglify = require('gulp-uglify-es').default;
+var pipeline = require('readable-stream').pipeline;
 
 gulp.task('default', function () {
   return gulp.src('./src/index.html')
@@ -12,4 +14,12 @@ gulp.task('css', () =>
   gulp.src('./css/*.css')
     .pipe(cleanCSS())
     .pipe(gulp.dest('css/'))
+);
+
+gulp.task('js', () =>
+  pipeline(
+    gulp.src('./js/*.js'),
+    uglify(),
+    gulp.dest('js/')
+  )
 );
