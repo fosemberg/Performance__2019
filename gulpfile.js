bf9b42065384688d4f5e590782a01c213797f582
fosemberg <fosemberg@gmail.com>
2019-10-22 23:05:18 +0300

    gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 const inlineCss = require('gulp-inline-css');
+let cleanCSS = require('gulp-clean-css');
 
-gulp.task('default', function() {
+gulp.task('default', function () {
   return gulp.src('./src/index.html')
     .pipe(inlineCss())
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('./'));
 });
+
+gulp.task('css', () =>
+  gulp.src('./css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('css/'))
+);
