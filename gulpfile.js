2466f6347968c3d969f8ba381305bc278594447b
Mikhail Bugakov <Mikhail.Bugakov@alpari.org>
2019-10-23 11:46:47 +0300

    revert: gulp-inline

--- gulpfile.js
+++ gulpfile.js
@@ -7,32 +7,33 @@ const htmlmin = require('gulp-htmlmin');
 const strip = require('gulp-strip-comments');
 const inline = require('gulp-inline');
 
+const destFolder = 'docs';
 
 gulp.task('html', () =>
   gulp.src('./src/index.html')
     .pipe(strip())
-    .pipe(inline({
+    // .pipe(inline({
       // base: 'public/',
-      js: uglify,
-      css: [cleanCSS],
-      disabledTypes: ['svg', 'img', 'js'], // Only inline css files
+      // js: uglify,
+      // css: [cleanCSS],
+      // disabledTypes: ['svg', 'img', 'js'], // Only inline css files
       // ignore: ['default.css', 'main.css']
-    }))
+    // }))
     // .pipe(inlineCss())
     // .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest(`${destFolder}/`))
 );
 
 gulp.task('css', () =>
   gulp.src('./css/*.css')
     .pipe(cleanCSS())
-    .pipe(gulp.dest('css/'))
+    .pipe(gulp.dest(`${destFolder}/css/`))
 );
 
 gulp.task('js', () =>
   pipeline(
     gulp.src('./js/*.js'),
     uglify(),
-    gulp.dest('js/')
+    gulp.dest(`${destFolder}/js/`)
   )
 );
